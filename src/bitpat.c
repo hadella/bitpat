5be8dc2114f61d90a7b1e8e266bfefd568518205
hadella <justin.hadella@gmail.com>
2023-05-16 14:45:38 -0600

    add the bitpat functions

--- src/bitpat.c
+++ src/bitpat.c
@@ -1,13 +1,18 @@
 /**
  * \file bitpat.c
  * \author Justin Hadella (justin.hadella@gmail.com)
- * \brief 
+ * \brief Implementation for utility functions for bit manipulation of integer numbers
  * \version 0.1
  * \date 2023-05-15
  * 
  * @copyright Copyright (c) 2023 Justin Hadella
  */
 #include "bitpat.h"
+
+#include <stdio.h>
+
+static unsigned int calc_mask(unsigned int bits, unsigned int n, unsigned int count);
+static unsigned int calc_pattern_mask(unsigned int pattern);
 
 int
 bit_test(unsigned int u, unsigned int n)
@@ -36,20 +41,58 @@ bit_toggle(unsigned int *u, unsigned int n)
 int
 bitpat_search(unsigned int u, unsigned int pattern, unsigned int n)
 {
-	// TODO
-	return 0;
+	int bits = 8 * sizeof(int);
+
+	unsigned int mask = calc_pattern_mask(pattern);
+
+	for (int i = n; i < bits; ++i) {
+		unsigned int tmp = (u >> i) & mask;
+
+		if (tmp == pattern) {
+			return i;
+		}
+	}
+
+	return -1;
 }
 
 unsigned int
 bitpat_get(unsigned int u, unsigned int n, unsigned int count)
 {
-	// TODO
-	return 0;
+	unsigned int mask = calc_mask(8 * sizeof(int), n, count);
+
+	return (u >> n) & mask;
 }
 
-int
+void
 bitpat_set(unsigned int *u,  unsigned int pattern, unsigned int n, unsigned int count)
 {
-	// TODO
-	return 0;
+	unsigned int mask = calc_mask(8 * sizeof(int), n, count);
+
+	// Clear bits within the mask
+	*u &= ~(mask << n);
+
+	// Set the pattern
+	*u |= (pattern & mask) << n;
+}
+
+static unsigned int
+calc_mask(unsigned int bits, unsigned int n, unsigned int count)
+{
+	unsigned bits_mask = (2 << ((bits - 1) - 1)) >> n;
+	unsigned count_mask = (2 << count) - 1;
+
+	return bits_mask & count_mask;
+}
+
+static unsigned int
+calc_pattern_mask(unsigned int pattern)
+{
+	unsigned int mask = 0;
+
+	for (int i = 0; mask < pattern; ++i) {
+		mask |= 1 << i;
+	}
+
+	return mask;
 }
--- src/bitpat.h
+++ src/bitpat.h
@@ -1,7 +1,7 @@
 /**
  * \file bitpat.h
  * \author Justin Hadella (justin.hadella@gmail.com)
- * \brief 
+ * \brief Definitions for utility functions for bit manipulation of integer numbers
  * \version 0.1
  * \date 2023-05-15
  * 
@@ -16,17 +16,21 @@ extern "C" {
 #endif
 
 /**
- * \brief Function to set whether or not a bit *n* in set in *u*
+ * \brief Predicate function indicates whether or not bit *n* in *u* is set
+ * 
+ * *n* == 0 represents the least significant bit
  * 
  * \param u  The number to test
  * \param n  The bit to test
- * \return 1 if the bit *n* is in *u* is set, otherwise 0
+ * \return 1 if bit *n* is in *u* is set, otherwise 0
  */
 int
 bit_test(unsigned int u, unsigned int n);
 
 /**
  * \brief Function to set bit *n* in *u*
+ * 
+ * *n* == 0 represents the least significant bit
  * 
  * \param u  The number to update
  * \param n  The bit to set
@@ -37,6 +41,8 @@ bit_set(unsigned int *u, unsigned int n);
 /**
  * \brief Function to clear bit *n* in *u*
  * 
+ * *n* == 0 represents the least significant bit
+ * 
  * \param u The number to update
  * \param n The bit to clear
  */
@@ -46,6 +52,8 @@ bit_clr(unsigned int *u, unsigned int n);
 /**
  * \brief Function to toggle bit *n* in *u*
  * 
+ * *n* == 0 represents the least significant bit
+ * 
  * \param u  The number to update
  * \param n  The bit to toggle
  */
@@ -53,38 +61,45 @@ void
 bit_toggle(unsigned int *u, unsigned int n);
 
 /**
- * \brief 
+ * \brief Function to search *u* for the *pattern* at an offset *n*
  * 
- * \param source 
- * \param pattern 
- * \param n 
- * \return int 
+ * *n* == 0 represents the least significant bit
+ * 
+ * \param source  the number to examine
+ * \param pattern the pattern to search for
+ * \param n       the index to start searching at
+ * 
+ * \return the index where the pattern is found, otherwise -1 if the pattern was not found
  */
 int
 bitpat_search(unsigned int u, unsigned int pattern, unsigned int n);
 
 /**
- * \brief 
+ * \brief Function gets the pattern of *count* bits from position *n* in *u*
  * 
- * \param u 
- * \param n 
- * \param count 
- * \return unsigned int 
+ * *n* == 0 represents the least significant bit
+ * 
+ * \param u     the number to extract the pattern from
+ * \param n     the bit position the pattern should start at
+ * \param count the number of bits in the pattern
+ * 
+ * \return the bit pattern found
  */
 unsigned int
 bitpat_get(unsigned int u, unsigned int n, unsigned int count);
 
 /**
- * \brief 
+ * \brief Function sets a specific set of bits in *u* with a *pattern*
  * 
- * \param u 
- * \param pattern 
- * \param n 
- * \param count 
- * \return int 
+ * *n* == 0 represents the least significant bit
+ * 
+ * \param u        the number to modify
+ * \param pattern  the pattern to set
+ * \param n        the bit position the pattern should start at
+ * \param count    the number of bits in the pattern
  */
-int
-bitpat_set(unsigned int *u,  unsigned int pattern, unsigned int n, unsigned int count);
+void
+bitpat_set(unsigned int *u, unsigned int pattern, unsigned int n, unsigned int count);
 
 #ifdef __cplusplus
 } 
